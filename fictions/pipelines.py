976bd4c74d7acfb89d3e34755c2f9086aeca12d1
Sinchard  Smith <a52581120@163.com>
2020-01-02 20:48:08 +0800

    Save the fiction item to the database

--- fictions/pipelines.py
+++ fictions/pipelines.py
@@ -4,8 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from myfiction.items import Fictions
 
-
-class FictionsPipeline(object):
+class MyfictionPipeline(object):
     def process_item(self, item, spider):
+        if Fictions.table_exists() == False:
+            Fictions.create_table()
+        try:
+            Fictions.create(fictionid=item['id'], chapterid=item['chapterid'], name=item['name'], content=item['content'])
+        except Exception as e:
+            if str(e.args[0]) == '1062':
+                print('重复数据，跳过。')
+            else:
+                print(e.args[0], e.args[1])
         return item
