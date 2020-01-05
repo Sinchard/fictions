566808bf52049794009a7a1b91669ce4c96c8d9c
Sinchard  Smith <a52581120@163.com>
2020-01-05 21:09:57 +0800

    According the item type, save the item to fiction table or content table

--- fictions/pipelines.py
+++ fictions/pipelines.py
@@ -4,19 +4,36 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-from myfiction.items import Fictions
+from fictions.items import Fictions, Chapters, Contents
+from fictions.items import FictionItem, ChapterItem, ContentItem
 
+
+def save_fiction(item):
+    if not Fictions.table_exists():
+        Fictions.create_table()
+    try:
+        Fictions.create(fiction_id=item['fiction_id'], url=item['url'], name=item['name'])
+    except Exception as e:
+        print(e.args[0], e.args[1])
+
+    return item
+
+
+def save_content(item):
+    if not Contents.table_exists():
+        Contents.create_table()
+    try:
+        Contents.create(fiction_id=item['fiction_id'], chapter_id=item['chapter_id'], url=item['url'], name=item['name'], content=item['content'])
+    except Exception as e:
+        print(e.args[0], e.args[1])
+
+    return item
 
 class MyfictionPipeline(object):
     def process_item(self, item, spider):
-        if Fictions.table_exists() == False:
-            Fictions.create_table()
-        try:
-            Fictions.create(fictionid=item['id'], chapterid=item['chapterid'], name=item['name'],
-                            content=item['content'])
-        except Exception as e:
-            if str(e.args[0]) == '1062':
-                print('�ظ����ݣ�������')
-            else:
-                print(e.args[0], e.args[1])
-        return item
+        if isinstance(item, FictionItem):
+            save_fiction(item)
+        elif isinstance(item, ContentItem):
+            save_content(item)
+
+
