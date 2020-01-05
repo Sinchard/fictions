1d78fb98fea5cbc9857ead9c4f75f04de3467301
Sinchard  Smith <a52581120@163.com>
2020-01-05 19:26:15 +0800

    Split old item into Fiction, Chapter and Content

--- fictions/items.py
+++ fictions/items.py
@@ -5,20 +5,45 @@ from peewee import *
 db = SqliteDatabase('fictions.db')
 
 
-# get the fiction name and url
-class FictionURLItem(scrapy.Item):
-    id = scrapy.Field()
+class MyItem(scrapy.Item):
     name = scrapy.Field()
-    chapterid = scrapy.Field()
-    content = scrapy.Field()
+    url = scrapy.Field()
 
 
-class Fictions(Model):
+class BaseModel(Model):
     id = AutoField()
-    fictionid = CharField(verbose_name="fictionid", max_length=100, null=False)
-    chapterid = CharField(verbose_name="chapterid", max_length=100, null=False)
     name = CharField(verbose_name="name", max_length=200, null=False)
-    content = TextField(verbose_name="content")
+    url = CharField(verbose_name="url", max_length=200, null=False)
 
     class Meta:
         database = db
+
+
+class FictionItem(MyItem):
+    fiction_id = scrapy.Field()
+
+
+class Fictions(BaseModel):
+    fiction_id = CharField(verbose_name="fiction_id", max_length=200, null=False)
+
+
+class ChapterItem(MyItem):
+    fiction_id = scrapy.Field()
+    chapter_id = scrapy.Field()
+
+
+class Chapters(BaseModel):
+    fiction_id = CharField(verbose_name="fiction_id", max_length=200, null=False)
+    chapter_id = CharField(verbose_name="chapter_id", max_length=100, null=False)
+
+
+# get the fiction name and url
+class ContentItem(MyItem):
+    chapter_id = scrapy.Field()
+    content = scrapy.Field()
+
+
+class Contents(BaseModel):
+    chapter_id = CharField(verbose_name="chapter_id", max_length=100, null=False)
+    content = TextField(verbose_name="content")
+
