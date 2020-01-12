8ec7a86445bbe6286407a4b67c158d855900d08e
Sinchard  Smith <a52581120@163.com>
2020-01-12 23:52:02 +0800

    Inserting many fictions and contents using cache and pool

--- fictions/pipelines.py
+++ fictions/pipelines.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
-
+import pymysql
+from DBUtils.PooledDB import PooledDB
+# 数据库pymysql的commit()和execute()在提交数据时，都是同步提交至数据库，由于scrapy框架数据的解析是异步多线程的，所以scrapy的数据解析速度，要远高于数据的写入数据库的速度。如果数据写入过慢，会造成数据库写入的阻塞，影响数据库写入的效率。
+# 通过多线程异步的形式对数据进行写入，可以提高数据的写入速度。
+from pymysql import cursors
+# 使用twsited异步IO框架，实现数据的异步写入。
+from twisted.enterprise import adbapi
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -39,3 +45,128 @@ class MyfictionPipeline(object):
             save_content(item)
 
         return item
+
+
+BUCK_FICTION_LENGTH = 1000
+BUCK_CONTENT_LENGTH = 50
+
+pool = PooledDB(creator=pymysql, maxcached=5, maxshared=5, host='localhost', user='root', passwd='123456',
+                db='fictions', port=3306, charset="utf8", setsession=['SET AUTOCOMMIT = 1'])
+
+
+class MySQLStorePipeline(object):
+    fiction_list = []
+    content_list = []
+    conn = None
+    cursor = None
+
+    def open_spider(self, spider):
+        pass
+
+    # 批量插入
+    def bulk_insert_fictions(self, fictions):
+        self.conn = pool.connection()
+        self.cursor = self.conn.cursor()
+        try:
+            print("inserting fictions in batch--->>>>>", len(fictions))
+            sql = """INSERT INTO fictions(name, fiction_id, url) VALUES (%s, %s, %s)"""
+            self.cursor.executemany(sql, fictions)
+            self.conn.commit()
+        except:
+            print("inserting fictions failed!")
+            self.conn.rollback()
+
+    def bulk_insert_contents(self, contents):
+        self.conn = pool.connection()
+        self.cursor = self.conn.cursor()
+        try:
+            print("inserting contents in batch--->>>>>", len(contents))
+            sql = """INSERT INTO contents(name, fiction_id, chapter_id, url, content) VALUES (%s, %s, %s, %s, %s)"""
+            self.cursor.executemany(sql, contents)
+            self.conn.commit()
+        except:
+            print("inserting contents failed!")
+            self.conn.rollback()
+
+    def process_item(self, item, spider):
+        if isinstance(item, FictionItem):
+            self.fiction_list.append((item['name'], item['fiction_id'], item['url']))
+            if len(self.fiction_list) >= BUCK_FICTION_LENGTH:
+                self.bulk_insert_fictions(self.fiction_list)
+                del self.fiction_list[:]
+        elif isinstance(item, ContentItem):
+            self.content_list.append(
+                (item['name'], item['fiction_id'], item['chapter_id'], item['url'], item['content']))
+            if len(self.content_list) >= BUCK_CONTENT_LENGTH:
+                self.bulk_insert_contents(self.content_list)
+                del self.content_list[:]
+
+        return item
+
+    # spider结束
+    def close_spider(self, spider):
+        print("closing spider,last commit")
+        self.bulk_insert_fictions(self.fiction_list)
+        self.bulk_insert_contents(self.content_list)
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+
+
+class MySQLTwistedPipeline(object):
+    """
+        MYSQL_HOST = 'localhost'
+        MYSQL_DB = 'fictions'
+        MYSQL_USER = 'root'
+        MYSQL_PASSWD = '123456'
+        MYSQL_CHARSET = 'utf8'
+        MYSQL_PORT = 3306
+    """
+
+    def __init__(self, dbpool):
+        self.dbpool = dbpool
+
+    @classmethod
+    def from_settings(cls, settings):
+        params = dict(
+            host=settings['MYSQL_HOST'],
+            db=settings['MYSQL_DB'],
+            user=settings['MYSQL_USER'],
+            passwd=settings['MYSQL_PASSWD'],
+            charset=settings['MYSQL_CHARSET'],
+            port=settings['MYSQL_PORT'],
+            cursorclass=cursors.DictCursor,
+        )
+        # 初始化数据库连接池(线程池)
+        # 参数一：mysql的驱动
+        # 参数二：连接mysql的配置信息
+        dbpool = adbapi.ConnectionPool('pymysql', **params)
+        return cls(dbpool)
+
+    def process_item(self, item, spider):
+        # 在该函数内，利用连接池对象，开始操作数据，将数据写入到数据库中。
+        # pool.map(self.insert_db, [1,2,3])
+        # 同步阻塞的方式： cursor.execute() commit()
+        # 异步非阻塞的方式
+        # 参数1：在异步任务中要执行的函数insert_db；
+        # 参数2：给该函数insert_db传递的参数
+        # query = self.dbpool.runInteraction(self.insert_db, item)
+
+        if isinstance(item, FictionItem):
+            query = self.dbpool.runInteraction(self.insert_fiction, item)
+        elif isinstance(item, ContentItem):
+            query = self.dbpool.runInteraction(self.insert_content, item)
+        # 如果异步任务执行失败的话，可以通过ErrBack()进行监听, 给insert_db添加一个执行失败的回调事件
+        query.addErrback(self.handle_error)
+        return item
+
+    def handle_error(self, field):
+        print('-----数据库写入失败：', field)
+
+    def insert_fiction(self, cursor, item):
+        insert_sql = "INSERT INTO fictions(name, fiction_id, url) VALUES (%s, %s, %s)"
+        cursor.execute(insert_sql, (item['name'], item['fiction_id'], item['url']))
+
+    def insert_content(self, cursor, item):
+        insert_sql = "INSERT INTO contents(name, fiction_id, chapter_id, url, content) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(insert_sql, (item['name'], item['fiction_id'], item['chapter_id'], item['url'], item['content']))
