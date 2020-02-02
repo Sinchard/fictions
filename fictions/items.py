632ab1155871150ea4c13192b41fdee66e47bddc
Sinchard Smith <a52581120@163.com>
2020-02-02 16:03:53 +0800

    Remove check and is_saved functions

--- fictions/items.py
+++ fictions/items.py
@@ -6,68 +6,22 @@ class MyItem(scrapy.Item):
     name = scrapy.Field()
     url = scrapy.Field()
 
-    def check(self):
-        return self.name is not None and self.url is not None
-
-    def is_saved(self):
-        pass
-
 
 class FictionItem(MyItem):
     fiction_id = scrapy.Field()
-    ifiction_id = scrapy.Field()
-
-    def check(self):
-        if super.check() and self.fiction_id is not None and self.ifiction_id > 0:
-            return self.fiction_id in self.url and int(self.fiction_id) == self.fiction_id
-        else:
-            return False
-
-    def is_saved(self):
-        sql = """SELECT * from fictions where ifiction_id={0:d}"""
-        sql = sql.format(self.ifiction_id)
-        self.cursor.execute(sql)
-        rows = self.cursor.fetchall()
-        return rows is not None and len(rows) > 0
+    save = scrapy.Field()
+    updated = scrapy.Field()
 
 
 class ChapterItem(MyItem):
     fiction_id = scrapy.Field()
     chapter_id = scrapy.Field()
-
-    def check(self):
-        if super.check() and self.fiction_id is not None and self.chapter_id is not None and self.ifiction_id > 0 and self.dchapter_id > 0:
-            return self.fiction_id in self.url and self.chapter_id in self.url and int(
-                self.fiction_id) == self.fiction_id and float(self.chapter_id) == self.dchapter_id
-        else:
-            return False
-
-    def is_saved(self):
-        sql = """SELECT * from contents where ifiction_id={0:d} and dchapter_id={1:f}"""
-        sql = sql.format(self.ifiction_id, self.dchapter_id)
-        self.cursor.execute(sql)
-        rows = self.cursor.fetchall()
-        return rows is not None and len(rows) > 0
+    updated = scrapy.Field()
 
 
-# get the fiction name and url
-class ContentItem(MyItem):
+class ContentItem(scrapy.Item):
     fiction_id = scrapy.Field()
     chapter_id = scrapy.Field()
-    ifiction_id = scrapy.Field()
-    dchapter_id = scrapy.Field()
+    #url = scrapy.Field()
     content = scrapy.Field()
-
-    def check(self):
-        if super.check() and self.fiction_id is not None and self.chapter_id is not None and self.ifiction_id > 0 and self.dchapter_id > 0:
-            return self.fiction_id in self.url and self.chapter_id in self.url and int(
-                self.fiction_id) == self.fiction_id and float(self.chapter_id) == self.dchapter_id
-        else:
-            return False
-
-    def is_saved(self):
-        sql = """SELECT * from contents where ifiction_id={0:d} and dchapter_id={1:f}"""
-        sql = sql.format(self.ifiction_id, self.dchapter_id)
-        self.cursor.execute(sql)
-        rows = self.cursor.fetchall()
-        return rows is not None and len(rows) > 0
+    updated = scrapy.Field()
