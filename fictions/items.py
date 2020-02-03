f021938bbb622845344756d5722aee1df49fbc9a
Sinchard Smith <a52581120@163.com>
2020-02-03 18:31:55 +0800

    add create class method to item classes, and remove getitem function in spider

--- fictions/items.py
+++ fictions/items.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 import scrapy
+
+from .settings import FICTION_URL
+
+
+def get_chapter_id(url):
+    return float(url.split("/")[-1].split(".")[-2].replace('_', '.'))
+
+
+def get_fiction_id(url):
+    return int(url.split("/")[-2])
 
 
 class MyItem(scrapy.Item):
@@ -12,16 +23,47 @@ class FictionItem(MyItem):
     save = scrapy.Field()
     updated = scrapy.Field()
 
+    @classmethod
+    def create(cls, fiction_a):
+        item = FictionItem()
+        url = fiction_a.xpath("a/@href").get()
+        item["fiction_id"] = get_fiction_id(url)
+        item["name"] = fiction_a.xpath("a/text()").get()
+        item["url"] = FICTION_URL.format(fiction_id)
+        item["save"] = 1
+        item["updated"] = datetime.now()
+        return item
+
 
 class ChapterItem(MyItem):
     fiction_id = scrapy.Field()
     chapter_id = scrapy.Field()
     updated = scrapy.Field()
 
+    @classmethod
+    def create(cls, chapter_a):
+        item = ChapterItem()
+        url = chapter_a.xpath("a/@href").get()
+        item["url"] = url
+        item["name"] = chapter_a.xpath("a/text()").get()
+        item["fiction_id"] = get_fiction_id(url)
+        item["chapter_id"] = get_chapter_id(url)
+        item["updated"] = datetime.now()
+        return item
+
 
 class ContentItem(scrapy.Item):
     fiction_id = scrapy.Field()
     chapter_id = scrapy.Field()
-    #url = scrapy.Field()
     content = scrapy.Field()
     updated = scrapy.Field()
+
+    @classmethod
+    def create(cls, response):
+        item = ContentItem()
+        url = response.url
+        item["fiction_id"] = get_fiction_id(url)
+        item["chapter_id"] = get_chapter_id(url)
+        item["content"] = response.xpath("//div[@id='nr1']").get().strip()
+        item["updated"] = datetime.now()
+        return item
--- fictions/spiders/fiction.py
+++ fictions/spiders/fiction.py
@@ -32,27 +32,10 @@ class FictionSpider(scrapy.Spider):
             self.log(url)
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def getContentItem(self, response):
-        if response is None or not isinstance(response, HtmlResponse):
-            return
-        content = ContentItem()
-        url = response.url
-        content["fiction_id"] = url.split("/")[-2]
-        content['chapter_id'] = url.split("/")[-1].split(".")[-2]
-        content["ifiction_id"] = int(content["fiction_id"])
-        content['dchapter_id'] = float(content['chapter_id'].replace('_', '.'))
-        title = response.xpath("//title/text()").get().strip()
-        content["name"] = title.split("_")[1]
-        content["url"] = url
-        # item['content'] = item.content.decode("gbk")
-        # item['content'] = item.content.encode("utf8")
-        content['content'] = response.xpath("//div[@id='nr1']").get().strip()
-        return content
-
     def parseContentURL(self, response):
         self.log("Response Encoding: %s" % response.encoding)
-        content = self.getContentItem(response)
-        if content['content'] is not None and content['content'] != "":
+        item = ContentItem.create(response)
+        if item['content'] is not None and item['content'] != "":
             yield content
         # get next page url
         next_page = response.xpath("//td[@class='next']/a/@href").get()
@@ -65,10 +48,11 @@ class FictionSpider(scrapy.Spider):
         print("Parsing Chapter Url:" + response.url)
         for c in response.xpath("//ul[@class='chapter']/li"):
             # get chapter content
-            url = c.xpath("a/@href").get()
-            fiction_id = int(url.split("/")[-2])
-            chapter_id = float(url.split("/")[-1].split(".")[-2])
-            if is_saved(ChapterModel, [fiction_id, chapter_id]):
+            item = ChapterItem.create(c)
+            if is_saved(ChapterModel,
+                        [item["fiction_id"], item["chapter_id"]]):
+                self.log("Fiction %d Chapter %d is saved" %
+                         (item['fiction_id'], item["chapter_id"]))
                 continue
             else:
                 if url is not None:
@@ -84,23 +68,11 @@ class FictionSpider(scrapy.Spider):
                                       callback=self.parseChapterUrl,
                                       priority=CHAPTER_PRIORITY)
 
-    def getFictionItem(self, f):
-        fiction = FictionItem()
-        url = f.xpath("a/@href").get()
-        fiction_id = url.split("/")[-2]
-        fiction["fiction_id"] = int(fiction_id)
-        fiction["name"] = f.xpath("a/text()").get()
-        url = FICTION_URL.format(fiction_id)
-        fiction["url"] = url
-        fiction['save'] = 1
-        fiction['updated'] = datetime.now()
-        return fiction
-
     # According to the settings param, get fiction url and parse the chapters' urls
     def parse(self, response):
         self.log(response.request.url)
         for f in response.xpath("//p[@class='line']"):
-            item = self.getFictionItem(f)
+            item = FictionItem.create(f)
             # Don't save the saved fiction
             if is_saved(FictionModel, item['fiction_id']):
                 self.log("Fiction %d is saved" % item['fiction_id'])
